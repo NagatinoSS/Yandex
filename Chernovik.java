d4cec07fae1a0e48f731e3095b31f288bf97b265
shishss <shishsse@yandex.ru>
2022-01-28 13:51:09 +0300

    Мой первый коммит!

---
+++ Chernovik.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Random;
+
+public class Chernovik {
+	public static Integer getNitroLevel(int nitroLevel) {
+		// Пропишите логику по конвертации параметра nitroLevel
+		if(nitroLevel!=0){
+
+			return nitroLevel;}
+		return null;
+
+	}
+	private static int generateInt(int from, int to) {
+		int diapason = to - from;
+		int offence = new Random().nextInt(diapason);
+		return from + offence;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(getNitroLevel(10));
+
+		float acceleration = generateInt(4, 10) / 10.0f;
+		System.out.println(acceleration);
+		HashMap<String, Double> orders = new HashMap<>();
+		orders.put("Иван И.", 4345.5);
+		orders.put("Ольга С.", 76564.43);
+		orders.put("Александр Т.", 1234.86);
+		orders.put("Александр Р.", 23432.87);
+		orders.put("Екатерина О.", 1034753.6);
+		orders.put("Ярослав В.", 450.0);
+
+		double sum =0.0; // Объявите переменную, где будет сохранена общая сумма
+		for (Double i : orders.values()) { // Пройдитесь в цикле по значениям
+			sum+=i;
+		}
+
+		System.out.println("Всего было совершено заказов на сумму: " + sum);
+
+	}
+
+
+}
---
+++ FinansAppVer4/ExpensesManager.java
@@ -0,0 +1,114 @@
+package FinansAppVer4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class ExpensesManager {
+	HashMap<String, ArrayList<Double>> expensesByCategories;
+
+	ExpensesManager() {
+		expensesByCategories = new HashMap<>();
+	}
+
+	double saveExpense(double moneyBeforeSalary, String category, double expense) {
+		moneyBeforeSalary = moneyBeforeSalary - expense;
+		System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
+		if (expensesByCategories.containsKey(category)) {
+			ArrayList<Double> expenses = expensesByCategories.get(category);
+			expenses.add(expense);
+		} else {
+			ArrayList<Double> expenses = new ArrayList<>();
+			expenses.add(expense);
+			expensesByCategories.put(category, expenses);
+		}
+		if (moneyBeforeSalary < 1000) {
+			System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
+		}
+		return moneyBeforeSalary;
+	}
+
+	void printAllExpensesByCategories() {
+		for (String category : expensesByCategories.keySet()) {
+			System.out.println(category);
+			ArrayList<Double> expenses = expensesByCategories.get(category);
+			for (Double expense : expenses) {
+				System.out.println(expense);
+			}
+		}
+	}
+
+	double findMaxExpenseInCategory(String category) {
+		double maxExpense = 0;
+		if (expensesByCategories.containsKey(category)) {
+			ArrayList<Double> expenses = expensesByCategories.get(category);
+			for (Double expense : expenses) {
+				if (expense > maxExpense) {
+					maxExpense = expense;
+				}
+			}
+		} else {
+			System.out.println("Такой категории пока нет.");
+		}
+		return maxExpense;
+	}
+
+	void removeAllExpenses() {
+		expensesByCategories.clear();
+		System.out.println("Траты удалены.");
+	}
+
+	// Напишите метод для получения суммы всех трат
+	double getExpensesSum() {
+		double sum = 0;
+		for (ArrayList<Double> exp : expensesByCategories.values()) { //обход по значениям
+			for (double d : exp) {
+				sum += d;
+			}
+		}
+		return sum;
+
+	}
+
+	// Напишите метод для удаления категории
+	void removeCategory(String remove){
+		if(expensesByCategories.containsKey(remove)){
+			expensesByCategories.remove(remove);
+		}
+	}
+
+
+	 // Напишите метод для получения категории, где размер трат больше всего
+	// Используйте эти переменные для сохранения промежуточных значений
+	 String getMaxOrderCustomerName() {
+		 double maxCategorySum = 0;
+		 String maxCategoryName = "";
+		for (String s : expensesByCategories.keySet()) {
+			ArrayList<Double> expenses = expensesByCategories.get(s);
+			double sum = 0;
+			for (Double d: expenses){
+				sum+=d;
+			}
+			if (sum>maxCategorySum){
+				maxCategorySum=sum;
+				maxCategoryName = s;
+			}
+		}
+		 // Здесь должен быть обход по значениям
+		/* for (String s : expensesByCategories.keySet()) {
+			 double sum = 0;
+			 for (double orderPrice : expensesByCategories.get(s)) {
+				 sum += orderPrice;
+			 }
+			 if (sum > maxCategorySum) {
+				 maxCategorySum = sum;
+				 maxCategoryName = s;
+			 }
+		 }*/
+
+		 return maxCategoryName;
+	 }
+
+
+
+}
